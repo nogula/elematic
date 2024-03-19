66a5a4018433050460e375a6298225398be7e1f0
nogula <noahrgula@gmail.com>
2024-03-18 22:49:14 -0500

    Moved tests outside of package

--- elematic/tests/tests.py
+++
@@ -1,8 +0,0 @@
-from elematic.api import importers
-from elematic.api import MatML_api as mapi
-
-inputFile = r"elematic\res\test.xml"
-doc = importers.import_xml(inputFile)
-
-material = doc.get_Material(id="M1001")
-print(material.BulkDetails.get_PropertyData(property="pr1")[0])
---
+++ tests/tests.py
@@ -0,0 +1,4 @@
+import os
+from elematic.api import io
+from elematic.api import MatML_api
+from elematic.api import utilities
